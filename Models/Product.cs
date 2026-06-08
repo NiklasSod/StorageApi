e8efbdca43674f051b9108fa99f3cc900fc9671f
NiklasSod <nma.prob@gmail.com>
2026-06-08 15:39:42 +0200

    updated price decimal sql rule

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,9 +1,12 @@
-﻿namespace StorageApi.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StorageApi.Models
 {
     public class Product
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
         public string? Category { get; set; }
         public string? Shelf { get; set; }
